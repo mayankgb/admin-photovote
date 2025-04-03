563a0271f3ec9928ae592f63a7f200e53fb81c6a
Mayank Agrawal <mayankagrawal@Mayanks-MacBook-Air.local>
2025-04-03 15:35:33 +0530

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
 "use client"
-
-import { signIn } from "next-auth/react";
-import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 import SignInPage from "./_component/SignInPage/SignInPage";
 
 export default function Home() {
-  const searchParams = useSearchParams()
-  const url = searchParams.get("callback") ?? "/home"
 
   return (
     <div className="flex justify-center items-center h-screen">
